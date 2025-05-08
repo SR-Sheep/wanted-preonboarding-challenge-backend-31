52ffc18c70cb379c9e4db034385d6b1ffd433481
YSR <act4ysr@gmail.com>
2025-05-08 17:32:00 +0900

    초기 설정 관련 파일 수정

---
+++ src/main/java/com/example/productApp/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.productApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/api/test")
+    public String test(){
+        return "test";
+    }
+
+}
